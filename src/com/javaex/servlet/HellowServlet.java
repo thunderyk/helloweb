837c7e5e308bb1ceb3d3ef45db3bc3e63fbea92c
dark <dark@DESKTOP-MTKP5N3>
2020-12-31 16:40:28 +0900

    servlet & jsp

---
+++ src/com/javaex/servlet/HellowServlet.java
@@ -0,0 +1,27 @@
+package com.javaex.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HellowServlet")
+public class HellowServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+    	response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>헬로우 월드 servlet</h1>");
+	}
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+	}
+
+}
